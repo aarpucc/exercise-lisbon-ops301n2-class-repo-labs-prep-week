86a4e081f38f291c46fdd2b6c7bb256262cd3038
Bruno Fernandes <162385760+aarpucc@users.noreply.github.com>
2024-03-07 13:29:52 +0000

    Add files via upload

---
+++ hello.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo 'Hello World'
+
+sleep 2
+
+echo 'no pain no game'
+
+sleep 3
+
+echo 'estou farto de bater com a cabeça'
+
+sleep 3
+
+echo 'abraço'
+
+
+# exercise-lisbon-ops301n2-class-repo-labs-prep-week
