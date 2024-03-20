8c573b04b78e12ec01fe549f7a6703daaf96f7ea
Bruno Fernandes <162385760+aarpucc@users.noreply.github.com>
2024-03-20 00:35:09 +0000

    Add files via upload

---
+++ challenge03.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Script: Ops301 Challenge 02
+# Purpose: input permissions number 777 
+# Why: Because it is important know when are script instructions created
+
+
+# texto informativo
+echo 'ao executar este ficheiro todos os users vão ter permissoes para ler escrever e executar'
+
+
+# lista as permissoes atuais 
+echo 'lista das pemissoes atuais'
+lista="ls -l"
+
+$lista
+
+#crira variavel para permissoes
+permissoes="777"
+
+# texto informativo
+echo exectuar o comando de forma aos ficheiros terem permissões totais
+
+# da permissoes totais nos ficheiros da diretoria onde e executado
+chmod $permissoes ./*
+
+# lista as permissoes 
+$lista
+
+
+
+
+
+
+
+
+
+
